9d66c7fc99fa847165eb2c3a30fbcf8f31d091e4
pedroleni <escaneruclm@gmail.com>
2024-01-10 11:58:05 +0100

    iteration#1.1

---
+++ JS/basicAlgorithms/app.js
@@ -0,0 +1,7 @@
+// 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+let myFavoriteHero = "Hulk";
+
+const sumar = (a, b) => a + b;
+
+const resultado = sumar(2, 2);
+//console.log(resultado);
